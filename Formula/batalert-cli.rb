811ce0e7d459ef8bda4fba8ce45be372cb8ffbec
elkwaet <elkwaet@gmail.com>
2026-03-20 11:51:07 +0000

    FIX: correct sha256 for v1.3.0 source archive

--- Formula/batalert-cli.rb
+++ Formula/batalert-cli.rb
@@ -14,7 +14,7 @@ class BatalertCli < Formula
   desc "CLI configuration tool for BatAlert — lightweight macOS battery monitor"
   homepage "https://gitlab.com/elkwaet/battery-alert-macos"
   url "https://gitlab.com/elkwaet/battery-alert-macos/-/archive/v1.2.0/battery-alert-macos-v1.2.0.tar.gz"
-  sha256 "bff7028bfbde4886b9800b6910ac59ceab74c96118ebe3ac5b9587ee02e5905d"
+  sha256 "924feedf1f027699f5fd2aee3d141f0aa8fef954f601087101fa7f39debb62b8"
   license "MIT"
   version "1.3.0"
 
